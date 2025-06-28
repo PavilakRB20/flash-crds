77a1f58ee31eac641f6088cd87c35b1647218563
PavilakRB20 <amanda.larsen@escola.pr.gov.br>
2025-06-27 21:44:36 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    "Fórmula1",
+    "Onde foi o primeiro GP?",
+    "Na Inglaterra."
+)
+
+criaCartao(
+    "Fórmula1",
+    "Em qual ano esse GP ocorreu?",
+    "Em 1950."
+)
+
+criaCartao(
+    "Fórmula1",
+    "Entre as equipes atuais, qual equipe participou de mais GPs?",
+    "A Ferrari."
+)
+
+criaCartao(
+    "Fórmula1",
+    "A zona de pontuação vai de qual posição até qual posição?",
+    "Da posição 10(menos pontos) até a 1(mais pontos)."
+)
+
+criaCartao(
+    "Fórmula1",
+    "Qual piloto do grid atual ganhou mais campeonatos mundiais?",
+    "Hamilton, com 7 campeonatos mundiais."
+)
+
+criaCartao(
+    "Fórmula1",
+    "Quantas equipes há no grid atual?",
+     "Há 10 equipes."
+)
